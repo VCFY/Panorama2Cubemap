06c691d6d18e95f6248ab3113a870a34dfec134f
VCFY <VCFY@users.noreply.github.com>
2017-07-14 08:41:20 -0700

    Update convert.js

--- convert.js
+++ convert.js
@@ -112,11 +112,11 @@ function copyPixelLanczos(read, write) {
     }
     else {
       const xp = Math.PI * x;
-      return 10 * Math.sin(xp) * Math.sin(xp / 10) / (xp * xp);
+      return 1 * Math.sin(xp) * Math.sin(xp / 1) / (xp * xp);
     }
   };
 
-  return kernelResample(read, write, 10, kernel);
+  return kernelResample(read, write, 1, kernel);
 }
 
 const orientations = {
